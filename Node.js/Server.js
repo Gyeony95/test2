0a9937af395590f24ac38a11dd8c4e56492b2099
Gyeony95 <rlg1133@naver.com>
2019-10-29 01:56:59 +0900

    1029 0156

--- Node.js/Server.js
+++ Node.js/Server.js
@@ -1,6 +1,6 @@
-var socketio = require('socket.io').listen(1234)
+var socketio = require('socket.io').listen(3000)
 var agents = require('./UserAgent.js')();
 var socketmsg = require('./SocketMsg.js')(socketio, agents);
 var colors = require('colors');
 
-console.log('EasyRTC Server listening on port 1234'.red);
+console.log('EasyRTC Server listening on port 3000'.red);
